83c05ff360c85ab0c27be90c57b7237653683805
Austin Conlon <austinconlon@icloud.com>
2020-04-08 18:22:12 -0700

    Initial Commit

---
+++ Try Haptics WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Try Haptics WatchKit Extension
+//
+//  Created by Austin Conlon on 4/8/20.
+//  Copyright © 2020 Austin Conlon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Try Haptics WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Try Haptics WatchKit Extension
+//
+//  Created by Austin Conlon on 4/8/20.
+//  Copyright © 2020 Austin Conlon. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
